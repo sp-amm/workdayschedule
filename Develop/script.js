d4321d3275202c64b8db14b31df0d24ba056abf5
sp <sam.pack.sp@gmail.com>
2020-09-15 22:12:28 +0930

    added input groups for user task lists

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,15 @@
 
 
 
-
+//Date in jumbotron
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+//Moving colours of scheduler to reflect current time
+
+var currentHour = moment().hour();
+console.log(currentHour);
+
+for (var i = 0; i<= 8; i++){
+    $("#"+currentHour).addClass(".present");
+    console.log("#"+currentHour)
+}
