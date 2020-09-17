23638f55f912956ca65d870ba3d59442ba72e158
sp <sam.pack.sp@gmail.com>
2020-09-17 20:36:44 +0930

    success in geting the table row id into variable for Planner hour to set the colour classes in the css

--- Develop/script.js
+++ Develop/script.js
@@ -8,8 +8,32 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
 var currentHour = moment().hour();
 console.log(currentHour);
+console.log(this);
+var plannerHour = $('table > tbody > tr').attr("id");
+console.log(plannerHour);
 
-for (var i = 0; i<= 8; i++){
-    $("#"+currentHour).addClass(".present");
-    console.log("#"+currentHour)
-}
+function pastPresentColours(){
+   
+    $('table > tbody > tr').each(function(){
+        
+     var plannerHour = $(this).attr("id");
+       console.log(plannerHour); 
+
+       if (plannerHour == currentHour){
+           $(this).addClass("present")}
+         else if ( plannerHour > currentHour){
+            $(this.addClass("past"))}
+            else {$(this.addClass("future"))}
+    })
+};
+
+
+//var currentTime = moment().get('hour')
+
+
+//})
+//for (var i = 0; i<= 8; i++){
+   // if $()
+   // $("#"+currentHour).addClass(".present");
+   // console.log("#"+currentHour)
+//}
