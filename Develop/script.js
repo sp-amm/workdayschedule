3510a80dab13f573c9b12be3b03261ac52dc623a
sp <sam.pack.sp@gmail.com>
2020-09-19 15:39:35 +0930

    added if function to clear at hour 12

--- Develop/script.js
+++ Develop/script.js
@@ -7,11 +7,13 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 //Moving colours of scheduler to reflect current time
 
 var currentHour = moment().hour();
+//var currentHour = 12;
 console.log(currentHour);
 
 
 function pastPresentColours(){
    
+ //Parent child selector info for the jquery loop from Fabrizio Calderan at https://stackoverflow.com/questions/10431987/jquery-each-loop-in-table-row https://api.jquery.com/child-selector/
     $('table > tbody > tr').each(function(){
         var plannerHour = $(this).attr("id");
         console.log(plannerHour);
@@ -30,28 +32,30 @@ function pastPresentColours(){
 
 pastPresentColours()
 
-//Saving calendar entries into local storage
-
-//console log to find the data
-$("button").on("click", function(event){console.log($(event.target).closest('tr').find('input').val())});
-
-$("button").on("click", function(event){console.log($(event.target).closest('tr').attr("id"))});
 
 //puts the scheduler data into the local storage
 $("button").on("click", function(event){
     localStorage.setItem($(event.target).closest('tr').attr("id"), $(event.target).closest('tr').find('input').val())});
 
-//Getting the values from local storage to ensure they are displayed after refresh
- $('table > tbody > tr').each(function(){
-    var task = localStorage.getItem($(this).attr("id")); 
-    console.log(task); 
-    $(this).find('input').val(task);
- })   
-   
+
+//Function for diplaying the local stoarge data (saved scheduler tasks) after refresh
+function showTasks(){ 
+
+    $('table > tbody > tr').each(function(){
+        var task = localStorage.getItem($(this).attr("id")); 
+        console.log(task); 
+        $(this).find('input').val(task);
+    });   
+};
+
+showTasks();
 
 //function to clear the local storage when the day ends after midnight
-//if(currentHour > 01 && currentHour > 08){
-  //  localStorage.clear()};
+if(currentHour === 12){
+   localStorage.clear()
+   showTasks();
+};
+
 
 });
 
